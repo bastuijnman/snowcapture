5128c1ce34e87fe83a1f4cb886933bfc7e8334b0
Bas Tuijnman <bas.tuijnman@24i.com>
2015-11-11 22:32:32 +0100

    use underscore templates

--- index.js
+++ index.js
@@ -4,13 +4,22 @@
     let capture = require('./src/capture'),
         timelapse = require('./src/timelapse'),
         express = require('express'),
+        consolidate = require('consolidate'),
+        lowdb = require('lowdb'),
+        db = lowdb('./db.json'),
         app = express(),
         server;
+
+    app.engine('html', consolidate.underscore);
+    app.set('view engine', 'html');
+    app.set('views', './src/views');
 
     app.use(express.static('./public'));
 
     app.get('/', function (req, res) {
-        res.sendfile('./src/views/index.html');
+        res.render('index', {
+            captures: []
+        });
     });
 
     server = app.listen(3000, function () {
